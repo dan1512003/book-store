cdfb2deaafec48ac17d1c00d80c5c4558e1b9b26
dan1512003 <nnguyengg123@gmail.com>
2026-09-11 10:50:48 +0700

    Add features structure

---
+++ src/app/features/books/pages/book-create/book-create.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-book-create',
+  styleUrl: './book-create.css',
+  templateUrl: './book-create.html',
+})
+export class BookCreate {}
---
+++ src/app/features/books/pages/book-detail/book-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-book-detail',
+  styleUrl: './book-detail.css',
+  templateUrl: './book-detail.html',
+})
+export class BookDetail {}
