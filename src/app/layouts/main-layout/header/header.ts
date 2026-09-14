83f680c92926c0432df4b3709cf17b43c700d0ba
dan1512003 <nnguyengg123@gmail.com>
2026-09-14 10:24:27 +0700

    design header and footer

--- src/app/layouts/main-layout/header/header.ts
+++ src/app/layouts/main-layout/header/header.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './header.css',
   templateUrl: './header.html',
 })
-export class Header {}
+export class Header {
+
+  activeMenu: string = 'home';
+
+}
