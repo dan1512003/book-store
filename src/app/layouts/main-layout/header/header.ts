e79abb3111b5dada9cb6b100c7bb3f887e2e73b9
dan1512003 <nnguyengg123@gmail.com>
2026-09-15 12:12:00 +0700

    implement responsive header and footer

--- src/app/layouts/main-layout/header/header.ts
+++ src/app/layouts/main-layout/header/header.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   imports: [],
   selector: 'app-header',
@@ -8,6 +9,25 @@ import { Component } from '@angular/core';
 })
 export class Header {
 
-  activeMenu: string = 'home';
+activeMenu: string = 'home';
+ activeItem: string = 'home';
+  isSideNavOpen:boolean = false;
+isSearchOpen:boolean= false;
 
+openSearch():void {
+  this.isSearchOpen=true
+}
+closeSearch(): void {
+    this.isSearchOpen = false;
+  }
+  openSideNav(): void {
+    this.isSideNavOpen = true;
+  }
+
+  closeSideNav(): void {
+    this.isSideNavOpen = false;
+  }
+  setActive(item: string): void {
+    this.activeItem = item;
+  }
 }
